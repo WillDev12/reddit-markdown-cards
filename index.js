4f6f4cc3239ef830ba28b0a21f9f8e1e29fab5d8
WillDev12 <willdevv12@gmail.com>
2023-05-28 18:59:42 -0400

    bugfix

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 ////////////////////////////////////////
 
 const express = require("express");
-const api2 = require("./name");
 const api = require("./api");
 const PORT = process.env.PORT || 8080;
 const app = express();
