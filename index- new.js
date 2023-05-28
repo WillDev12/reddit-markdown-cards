6ab78c66f5460e478cc06dfeaf86e0a46a9511a4
WillDevv12 <93289932+WillDev12@users.noreply.github.com>
2023-05-28 10:29:06 -0400

    test

---
+++ index- new.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const api = require("./name");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.get('/', (req, res, next) => {
+    res.redirect('https://github.com/WillDev12');
+});
+
+app.use('/api', api);
+
+app.listen(PORT, () => {console.log('listening on 8080')});
+
+module.exports = app;
