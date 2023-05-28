40e82f3368f9e7027f6d9fe6159414e89106c081
WillDev12 <willdevv12@gmail.com>
2023-05-28 19:10:28 -0400

    bugfix

--- api.js
+++ api.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.route("/").get(nameReply).post(nameReply);
 
 function nameReply(req, res) {
+  res.set('Content-Type', 'image/svg');
   const method = req.method;
   const requestType = method === "GET" ? req.query.type : req.body.type;
   const nameValue = method === "GET" ? req.query.name : req.body.name;
