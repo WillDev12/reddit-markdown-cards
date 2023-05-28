806a7549748500e2ef24a06157f60fbd8472a328
WillDev12 <willdevv12@gmail.com>
2023-05-28 18:56:41 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # reddit-markdown-cards
+
 A node.js web api that provides markdown cards for your Reddit account
--- index.js
+++ index.js
@@ -11,14 +11,16 @@ const api = require("./api");
 const PORT = process.env.PORT || 8080;
 const app = express();
 
-app.get('/', (req, res, next) => {
-    res.redirect('https://github.com/WillDev12/reddit-markdown-cards');
+app.get("/", (req, res, next) => {
+  res.redirect("https://github.com/WillDev12/reddit-markdown-cards");
 });
 
-app.use('/apitest', api2);
+app.use("/apitest", api2);
 
-app.use('/api', api);
+app.use("/api", api);
 
-app.listen(PORT, () => {console.log('listening on 8080')});
+app.listen(PORT, () => {
+  console.log("listening on 8080");
+});
 
 module.exports = app;
